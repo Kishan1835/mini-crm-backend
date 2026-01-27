e5e26f9d621aadea8b7725053fe5d2cab91f50ca
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2026-01-28 02:18:09 +0530

    correcting import tasks to task

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,8 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { CustomersModule } from './customers/customers.module';
-import { TasksModule } from './tasks/tasks.module';
-import { AppController } from './app.controller';
+import { TasksModule } from './task/tasks.module';
+import { AppController } from './app.controller';  // Add this import
 
 @Module({
   imports: [
@@ -18,6 +18,6 @@ import { AppController } from './app.controller';
     CustomersModule,
     TasksModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController],  // Add this line
 })
 export class AppModule {}
